32c746762c799a59232a850a72230193e3227225
Gillian Wenhold <gillian.wenhold@powerhrg.com>
2021-06-07 10:07:29 -0400

    Added the rest of the toy info to each card

--- src/index.js
+++ src/index.js
@@ -2,18 +2,34 @@ let addToy = false;
 let toyContainer = document.querySelector("#toy-collection") // target the element/container for each card to land
 
 const addToyCard = (toy) => { // Add toy cards to the DOM
-
   let newToyCard = document.createElement("div") // Create a div for the card info to land
   newToyCard.classList.add("card") // Added "card" to the list of classes for the div
 
   // - h2 tag with the toy.name
+  let h2 = document.createElement("h2") // Create empty element
+  h2.innerText = toy.name // Set the innerText to the toy.name
+  newToyCard.append(h2) // Append h2 to the card
+
   // - img tag with the src of toy.image
   //   and the className = "toy-avatar"
+  let img = document.createElement("img") // Create empty element
+  img.src = toy.image // set the src of the img to toy.image
+  img.classList.add("toy-avatar") // Add class of "toy-avatar"
+  newToyCard.append(img) // Append img to the card
+
   // - p tag with toy.likes
+  let p = document.createElement("p") // Create empty element
+  p.innerText = `${toy.likes} likes` // Set text of paragraph to toy.likes
+  newToyCard.append(p) // Append paragraph to the card
+
   // - button tag with a class "like-btn" and
   //   an id attribute set to toy.id
+  let btn = document.createElement("button") // Create empty element
+  btn.classList.add("like-btn") // Set the class to "like-btn"
+  btn.setAttribute("id", toy.id) // Set the ID of the button
+  btn.innerText = "Like"
+  newToyCard.append(btn) // Append button to the card
 
-  newToyCard.innerHTML = toy.name // THIS IS TEMPORARY JUST TO SEE IF IT WORKS
   toyContainer.append(newToyCard) // Appending the toy card to the div container
 }
 
@@ -63,5 +79,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
 FIRST COMMIT: https://github.com/powercodeacademy/phase-1-challenge-toy-tale/commit/2102aa1a2f459eb942421cd2bb3372030e7fd533
 SECOND COMMIT: https://github.com/powercodeacademy/phase-1-challenge-toy-tale/commit/65ec38bc2665bb8853dce0fb12aedd3579b1b7a5
+THIRD COMMIT: https://github.com/powercodeacademy/phase-1-challenge-toy-tale/commit/d6d36eaf048dda8e6bf0ddf57693a4cb5c5a041e
 
 */
